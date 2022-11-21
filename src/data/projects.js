52ebbcee1726808cd0415b56dbec991a0c3c6494
laurenhatchett <laurenhatchett@gmail.com>
2022-11-21 07:58:36 -0500

    update project data descriptions

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,33 +1,33 @@
 const projects = [
 {
   title: 'Tic Tac Toe',
-  description: 'Tic Tac Toe',
+  description: 'A fun and interactive game of Tic-Tac-Toe built with JavaScript, HTML, and CSS. ',
   image: 'ticTacToe.png',
   repositoryLink: 'https://github.com/laurenhatchett/ttt-weekend',
   deploymentLink: 'https://lauren-hatchett-tic-tac-toe.netlify.app'
 },
 {
   title: 'Connect Four',
-  description: 'Connect Four',
+  description: 'An exciting game that you can play with two players. This game was built using JavaScript, HTML, and CSS. ',
   image: 'connectFour.png',
   repositoryLink: 'https://github.com/laurenhatchett/Connect-Four-Game.git',
   deploymentLink: 'https://lauren-hatchett-connect-four-game.netlify.app/'
 },
 {
   title: 'Festive',
-  description: "Festive",
+  description: "Festive is an interactive website designed to help users get inspired for the holidays. Users are able to share holiday ideas through adding and sharing posts. This is a MEN Stack Application that was built with CSS, EJS, and MongoDB. ",
   image: 'festive.png',
   repositoryLink: 'https://github.com/laurenhatchett/festive.git',
   deploymentLink: 'https://festive.fly.dev/'
 },
 {title: 'WERK',
-description: "WERK",
+description: "WERK is a fun web application designed for job seekers in the tech world. This app offers a variety of features to help job seekers stay organized in one space. This is a full-stack MERN application that uses ReactJS, Node.js, Express, MongoDB, and CSS.",
 image: 'WERK.png',
 repositoryLink: 'https://github.com/ljchu87/werk-front-end.git',
 deploymentLink: 'https://werk-smarter.netlify.app/'
 },
 {title: 'Finch Collector',
-description: "Finch Collector",
+description: "This cool application is used to record or collect the birds that you see. This app was built with Python, HTML, CSS, and Django.  ",
 image: 'finchCollector.png',
 repositoryLink: 'https://github.com/laurenhatchett/finch-collector.git',
 deploymentLink: 'https://laurenfinchcollector.herokuapp.com/'
