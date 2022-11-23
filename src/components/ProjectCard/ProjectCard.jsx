d795dc887562badedc1f033491b733f70eb14108
laurenhatchett <laurenhatchett@gmail.com>
2022-11-23 01:43:30 -0500

    adjust ProjectCard component and update CSS styling

--- src/components/ProjectCard/ProjectCard.jsx
+++ src/components/ProjectCard/ProjectCard.jsx
@@ -1,34 +1,45 @@
-import { Link } from 'react-router-dom'
-import hyphenateWords from '../../utilities/hyphenateWords'
+// import { Link } from 'react-router-dom'
+// import hyphenateWords from '../../utilities/hyphenateWords'
 import styles from '../ProjectCard/ProjectCard.module.css'
 
 
 
 function ProjectCard (props) {
-  const path = hyphenateWords(props.title)
+  // const path = hyphenateWords(props.title)
 
   return (
     <>
-      <Link to={path}>
-        <div className={styles.container}>
-          <div className={styles.image}>
-          <img 
-            src={props.image} 
-            alt={props.title}
-            className={styles.cardImage}
-          />
+      {/* <Link to={path}> */}
+      <div className={styles.grid}>
+        <div className={styles.card}>
+
+          <div className={styles.cardimage}>
+            <img 
+              src={props.image} 
+              alt={props.title}
+              className={styles.image}
+            />
           </div>
 
           <div className={styles.body}>
             <h2>
               {props.title}
             </h2>
-			      <button>
-              Learn more
-            </button>
+          </div>
+
+          <div className={styles.footer}>
+            
+			        <a href={props.repositoryLink}>
+                <button className={styles.button}>GitHub Repository</button>
+              </a>
+              <a href={props.deploymentLink}>
+                <button className={styles.button}>Deployed Site</button>
+              </a>
+            
+          </div>
         </div>
-      </div>
-    </Link>
+      </div> 
+    {/* </Link> */}
     </>
   )  
 }
