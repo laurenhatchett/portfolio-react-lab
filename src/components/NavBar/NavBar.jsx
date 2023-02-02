52554702af2f548b902cee572b021967ae8f80d9
laurenhatchett <laurenhatchett@gmail.com>
2023-02-02 10:16:11 -0500

    stub up handleClick function in NavBar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,21 +1,16 @@
 import styles from './NavBar.module.css'
 
 
-
-// const toggleButton = document.getElementsByClassName('toggleButton')[0]
-// const links = document.getElementsByClassName('links')[0]
-
-// toggleButton.addEventListener('click', () => {
-//   links.classList.toggle('active')
-// })
-
 function NavBar() {
   
+  const handleClick = () => {
+    console.log ('toggle was clicked!')
+  }
 
   return (
       <nav className={styles.navbar}>
         <div className={styles.brand}>LH</div>
-          <a href='#toggle' className={styles.toggleButton}>
+          <a onClick={handleClick} href='#toggle' className={styles.toggleButton}>
             <span className={styles.bar}></span>
             <span className={styles.bar}></span>
             <span className={styles.bar}></span>
